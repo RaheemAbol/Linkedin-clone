7f1d8137242d16482c1eec8e4d5245f738a559cc
Raheem Abolfathzadeh <72184069+RaheemAbol@users.noreply.github.com>
2021-02-08 15:58:32 -0500

    Update App.js
    Added Comments

--- src/App.js
+++ src/App.js
@@ -9,6 +9,15 @@ import Login from './Login';
 import Sidebar from './Sidebar';
 import Widgets from './Widgets';
 
+// The App Body will be setup with three different sections sidebar, feed , and widgets.\\
+// In our App we pull the user from the store useing the useSelector() and passing it selectUser \\
+// Before the login component we set up an if statement that says if there is no user then display the login page otherwise render the rest of the app.\\
+// we create a useEffect that takes in our auth and passed it the method onAuthStateChanged() which is then passed the userAuth.\\
+//^ if the userAuth is there then the user is logged in. Else the user is logged out. \\
+// if the user is logged in we will dispatch it to the store useing our useDispatch hook \\
+// in our dispatch we import our login and the set (email,uid,displayName,photoUrl) inside of our redux store to userAuth.email userAuth.uid etc...\\
+
+
 
 function App() {
 const user = useSelector(selectUser);
