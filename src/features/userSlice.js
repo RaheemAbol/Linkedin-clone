5b9a72fbd63e08bd57a860e74c6d510652ca576c
Raheem Abolfathzadeh <72184069+RaheemAbol@users.noreply.github.com>
2021-02-08 15:57:35 -0500

    Update userSlice.js
    added comments

--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -1,5 +1,10 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+// Inside of userSlice we will store the user as an object and set its value to null\\
+// To avoid prop drilling we pass the user into the data layer by creating a slice with createSlice \\
+// Inside of our reducers we have two actions Login & Logout \\
+// our selector selectUser will allow us to pull the user info other places \\
+
 export const userSlice = createSlice({
   name: 'user',
   initialState: {
